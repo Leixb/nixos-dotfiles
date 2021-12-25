4f7560b98a314c0bddaec682e170524988aced11
LeixB <abone9999@gmail.com>
2021-12-25 20:05:53 +0100

    Update locale

--- system/lenovo/configuration.nix
+++ system/lenovo/configuration.nix
@@ -117,6 +117,12 @@ in
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_TIME = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+  };
+
   console = {
     font = "Lat2-Terminus16";
     useXkbConfig = true;
