e452ead6e1167df1e10536c15a8521e764181533
LeixB <abone9999@gmail.com>
2022-01-13 15:12:51 +0100

    Add nix-community cache

--- system/lenovo/configuration.nix
+++ system/lenovo/configuration.nix
@@ -328,6 +328,15 @@ in
     autoOptimiseStore = true;
     trustedUsers = [ "root" "leix" ];
     gc.automatic = true;
+
+    binaryCaches = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+
+    binaryCachePublicKeys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
 
