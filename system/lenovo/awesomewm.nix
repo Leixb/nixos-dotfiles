f885c908e36e85604735d4f75a0f3ffe3a05a2d3
LeixB <abone9999@gmail.com>
2022-03-27 14:43:28 +0200

    Configure Caps_lock remap

--- system/lenovo/awesomewm.nix
+++ system/lenovo/awesomewm.nix
@@ -1,39 +1,51 @@
+{ pkgs, ... }:
+
 {
   services.xserver = {
     enable = true;
     exportConfiguration = true;
 
-    displayManager.lightdm.enable = true;
-    displayManager.autoLogin.enable = false;
-    displayManager.autoLogin.user = "leix";
+    displayManager = {
 
-    displayManager.defaultSession = "xsession";
-    displayManager.session = [{
-      manage = "desktop";
-      name = "xsession";
-      start = "exec $HOME/.xsession";
-    }];
+      autoLogin.enable = false;
+      autoLogin.user = "leix";
 
-    displayManager.lightdm.greeters.mini = {
-        enable = true;
-        user = "leix";
-        extraConfig = ''
-            [greeter]
-            show-password-label = false
-            password-alignment = center
-            [greeter-theme]
-            background-image = "${../../users/leix/wallpapers/forest.jpg}"
-            font = "Fira Mono"
-            text-color = "#DDDDFF"
-            error-color = "#EA6F81"
-            background-color = "#1A1A1A"
-            window-color = "#313131"
-            border-color = "#313131"
-            password-color = "#82aaff"
-            password-background-color = "#1d3b53"
-            password-border-color = "#1d3b53"
-            sys-info-color = "#82aaff"
+      defaultSession = "xsession";
+      session = [{
+        manage = "desktop";
+        name = "xsession";
+        start = "exec $HOME/.xsession";
+      }];
+
+      sessionCommands = let 
+        xmodmap_config = pkgs.writeText "xkb-layout" ''
+          clear lock
+          keycode 66 = F13 F13 F13 F13
         '';
+      in "${pkgs.xorg.xmodmap}/bin/xmodmap ${xmodmap_config}";
+
+      lightdm.enable = true;
+      lightdm.greeters.mini = {
+          enable = true;
+          user = "leix";
+          extraConfig = ''
+              [greeter]
+              show-password-label = false
+              password-alignment = center
+              [greeter-theme]
+              background-image = "${../../users/leix/wallpapers/forest.jpg}"
+              font = "Fira Mono"
+              text-color = "#DDDDFF"
+              error-color = "#EA6F81"
+              background-color = "#1A1A1A"
+              window-color = "#313131"
+              border-color = "#313131"
+              password-color = "#82aaff"
+              password-background-color = "#1d3b53"
+              password-border-color = "#1d3b53"
+              sys-info-color = "#82aaff"
+          '';
+      };
     };
 
     # Configure keymap in X11
