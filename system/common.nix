27c95c59bccc747c60007d9419c3be27d1258639
LeixB <abone9999@gmail.com>
2022-04-09 16:27:36 +0200

    Go back to earlyoom

--- system/common.nix
+++ system/common.nix
@@ -41,31 +41,10 @@ in
   services.fstrim.enable = true;
   services.irqbalance.enable = true;
 
-  systemd.package = pkgs.systemd.override { withOomd = true; };
-  systemd.additionalUpstreamSystemUnits = [ "systemd-oomd.service" ];
-
-  # oomd requires swap to be configured before it can run;
-  # need to check for swapDevices mostly so that vm doesn't fail.
-  systemd.services.systemd-oomd = {
-    wantedBy = lib.mkIf (config.swapDevices != [ ]) [ "multi-user.target" ];
-    after = [ "swap.target" ];
-    aliases = [ "dbus-org.freedesktop.oom1.service" ];
+  services.earlyoom = {
+    enable = true;
+    freeMemThreshold = 2;
   };
-
-  systemd.sockets.systemd-oomd = {
-    listenStreams = [ "/run/systemd-oomd.socket" ];
-    wantedBy = [ "sockets.target" ];
-  };
-
-  # ids should be replaced eventually with something sane
-  users.groups.systemd-oom.gid = 666;
-  users.users.systemd-oom = {
-    uid = 666;
-    group = "systemd-oom";
-    isSystemUser = true; # seems to be required now
-  };
-
-  systemd.slices."-".sliceConfig.ManagedOOMSwap = "kill";
 
   services.ananicy = {
     enable = true;
