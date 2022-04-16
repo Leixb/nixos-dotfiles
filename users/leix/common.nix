e2f6fea786ed853596188ad077f64656c4b33b8b
LeixB <abone9999@gmail.com>
2022-04-16 13:35:05 +0200

    starship nix flake symbol without extra space

--- users/leix/common.nix
+++ users/leix/common.nix
@@ -278,6 +278,13 @@ in
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+
+    settings = {
+      nix_shell = {
+        symbol = "❄️ ";
+      };
+    };
+
   };
 
   programs.firefox = {
