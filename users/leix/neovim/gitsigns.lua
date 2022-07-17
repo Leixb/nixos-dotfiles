0f99b116288c3ca8cb561a521fbfe92e63dca8c8
LeixB <abone9999@gmail.com>
2022-07-17 22:17:21 +0200

    nvim: Silence gitsigns stage keymaps

--- users/leix/neovim/gitsigns.lua
+++ users/leix/neovim/gitsigns.lua
@@ -30,8 +30,8 @@ require("gitsigns").setup({
 		end, { expr = true, desc = "Previous hunk" })
 
 		-- Actions
-		map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
-		map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
+		map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk", silent = true })
+		map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk", silent = true })
 		map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage buffer" })
 		map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
 		map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset buffer" })
