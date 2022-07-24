e631d1ecd1590e4bdab9373d8dc66bf675aba699
LeixB <abone9999@gmail.com>
2022-07-24 17:12:08 +0200

    nvim: Use indexed `vim.cmd`

--- users/leix/neovim.nix
+++ users/leix/neovim.nix
@@ -115,7 +115,7 @@ in {
         type = "lua";
         config = ''
           vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-          vim.cmd[[colorscheme catppuccin]]
+          vim.cmd.colorscheme("catppuccin")
         '';
       }
 
