62a3de1505e7acbe19a4b760af826bba253d659c
LeixB <abone9999@gmail.com>
2022-09-09 14:48:23 +0200

    nvim: configure autosession

--- users/leix/neovim/auto-session.lua
+++ users/leix/neovim/auto-session.lua
@@ -1,4 +1,6 @@
 require("auto-session").setup({
 	log_level = "info",
-	auto_session_suppress_dirs = { "~/" },
+    auto_session_create_enabled = false,
+    auto_session_allowed_dirs = { "~/Documents" },
+    auto_session_use_git_branch = true,
 })
