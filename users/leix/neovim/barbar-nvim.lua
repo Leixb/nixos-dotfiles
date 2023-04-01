36ee6611f301b4eb9950c41689978ab889e68934
LeixB <abone9999@gmail.com>
2023-04-01 12:58:15 +0200

    Update flake and fix barbar

--- users/leix/neovim/barbar-nvim.lua
+++ users/leix/neovim/barbar-nvim.lua
@@ -1,4 +1,8 @@
-vim.g.bufferline = { closable = false }
+require'bufferline'.setup {
+    icons = {
+        button = '';
+    },
+}
 
 vim.keymap.set("n", "<F2>", "<cmd>BufferPrevious<CR>", { noremap = true, silent = true, desc = "BufferPrevious" })
 vim.keymap.set("n", "<F3>", "<cmd>BufferNext<CR>", { noremap = true, silent = true, desc = "BufferNext" })
