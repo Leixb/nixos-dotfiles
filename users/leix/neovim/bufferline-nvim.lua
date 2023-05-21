82007a7f762729a0a3ada78482de921e12c28396
LeixB <abone9999@gmail.com>
2023-05-22 00:58:40 +0200

    neovim: bufferline diagnostic icons

--- users/leix/neovim/bufferline-nvim.lua
+++ users/leix/neovim/bufferline-nvim.lua
@@ -5,6 +5,15 @@ bufferline.setup {
         diagnostics = "nvim_lsp",
         show_close_icon = false,
         show_buffer_close_icons = false,
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local s = " "
+          for e, n in pairs(diagnostics_dict) do
+            local sym = e == "error" and " "
+              or (e == "warning" and " " or "" )
+            s = s .. n .. sym
+          end
+          return s
+        end
     }
 }
 
