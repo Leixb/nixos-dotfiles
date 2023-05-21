385e700d4b92bfb80414ca8367a586049d631327
LeixB <abone9999@gmail.com>
2023-05-22 01:03:03 +0200

    neovim: fix bufferline cycle key

--- users/leix/neovim/bufferline-nvim.lua
+++ users/leix/neovim/bufferline-nvim.lua
@@ -18,5 +18,5 @@ bufferline.setup {
 }
 
 vim.keymap.set("n", "<F2>", "<cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
-vim.keymap.set("n", "<F3>", "<cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = true, desc = "Next buffer" })
+vim.keymap.set("n", "<F3>", "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bd", "<cmd>BufferLinePickClose<CR>", { noremap = true, silent = true, desc = "Close buffer" })
