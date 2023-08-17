fc7f42f157ba3e7ae6c13fb3255b2387256f9e32
LeixB <abone9999@gmail.com>
2023-08-17 13:54:03 +0200

    firefox: privacy settings

--- users/leix/firefox.nix
+++ users/leix/firefox.nix
@@ -5,6 +5,7 @@
     # package = pkgs.stable.firefox;
 
     profiles.leix = {
+      isDefault = true;
       settings = {
         "media.ffmpeg.vaapi.enabled" = true;
         "media.rdd-ffmpeg.enabled" = true;
@@ -13,14 +14,48 @@
         "gfx.x11-egl.force-enabled" = true;
         "gfx.webrender.all" = true;
 
-        "browser.newtabpage.activity-stream.showSponsored" = false;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-
         "browser.uidensity" = 1;
 
         "network.proxy.allow_hijacking_localhost" = true;
 
         # "privacy.webrtc.legacyGlobalIndicator" = false;
+        "browser.startup.homepage" = "https://start.duckduckgo.com";
+
+        "app.shield.optoutstudies.enabled" = false;
+        "app.update.auto" = false;
+        "browser.bookmarks.restore_default_bookmarks" = false;
+        "browser.contentblocking.category" = "strict";
+        "browser.ctrlTab.recentlyUsedOrder" = false;
+        "browser.discovery.enabled" = false;
+        "browser.laterrun.enabled" = false;
+        "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" =
+          false;
+        "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" =
+          false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = false;
+        "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned" = "";
+        "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines" = "";
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" =
+          false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.pinned" = false;
+        "browser.protections_panel.infoMessage.seen" = true;
+        "browser.quitShortcut.disabled" = true;
+        "browser.shell.checkDefaultBrowser" = false;
+        "browser.ssb.enabled" = true;
+        "browser.urlbar.placeholderName" = "DuckDuckGo";
+        "browser.urlbar.suggest.openpage" = false;
+        "datareporting.policy.dataSubmissionEnable" = false;
+        "datareporting.policy.dataSubmissionPolicyAcceptedVersion" = 2;
+        "dom.security.https_only_mode" = true;
+        "dom.security.https_only_mode_ever_enabled" = true;
+        "extensions.getAddons.showPane" = false;
+        "extensions.htmlaboutaddons.recommendations.enabled" = false;
+        "extensions.pocket.enabled" = false;
+        "identity.fxaccounts.enabled" = false;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
       };
 
       extensions = with pkgs.firefox-addons; [
@@ -34,6 +69,7 @@
         refined-github
         ublock-origin
         sponsorblock
+        vimium
         # tree-style-tab
       ];
 
