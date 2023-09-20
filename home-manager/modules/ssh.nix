9f912df91a436a2db0a3ebb51930561958e62d1d
LeixB <abone9999@gmail.com>
2023-09-20 11:39:40 +0200

    ssh: update home config

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,16 +1,14 @@
 # vim: sw=2 ts=2:
 { config, lib, pkgs, osConfig, system, inputs, ... }:
 {
-  sops.secrets.ssh_config_home_lan.path = "${config.home.homeDirectory}/.ssh/config.d/home_lan";
-  sops.secrets.ssh_config_home_wan.path = "${config.home.homeDirectory}/.ssh/config.d/home_wan";
+  sops.secrets.ssh_config_home.path = "${config.home.homeDirectory}/.ssh/config.d/home";
   sops.secrets.ssh_config_nas_lan.path = "${config.home.homeDirectory}/.ssh/config.d/nas_lan";
 
   programs.ssh = {
     enable = true;
 
     includes = [
-      config.sops.secrets.ssh_config_home_lan.path
-      config.sops.secrets.ssh_config_home_wan.path
+      config.sops.secrets.ssh_config_home.path
       config.sops.secrets.ssh_config_nas_lan.path
     ];
   };
