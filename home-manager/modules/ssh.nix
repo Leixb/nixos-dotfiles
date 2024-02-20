7faf7341c40fdb6f806cf22fcfe5100c9403840c
LeixB <abone9999@gmail.com>
2024-02-20 18:48:55 +0100

    Add ssh config for mc server

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -4,6 +4,7 @@
   sops.secrets.ssh_config_home.path = "${config.home.homeDirectory}/.ssh/config.d/home";
   sops.secrets.ssh_config_nas_lan.path = "${config.home.homeDirectory}/.ssh/config.d/nas_lan";
   sops.secrets.ssh_config_router.path = "${config.home.homeDirectory}/.ssh/config.d/router";
+  sops.secrets.ssh_config_mc.path = "${config.home.homeDirectory}/.ssh/config.d/mc";
 
   programs.ssh = {
     enable = true;
@@ -12,6 +13,7 @@
       config.sops.secrets.ssh_config_home.path
       config.sops.secrets.ssh_config_nas_lan.path
       config.sops.secrets.ssh_config_router.path
+      config.sops.secrets.ssh_config_mc.path
     ];
   };
 }
