c2886ab7853585c61f0c91e7136c6cb390883250
LeixB <abone9999@gmail.com>
2024-06-05 23:15:33 +0200

    packages: wrap paraver

--- home-manager/modules/bsc/bsc.nix
+++ home-manager/modules/bsc/bsc.nix
@@ -1,7 +1,23 @@
 { config, lib, pkgs, ... }:
+let
+
+  paraver = pkgs.wxparaver.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.copyDesktopItems ];
+  });
+
+  wxparaver-adawaita = pkgs.symlinkJoin {
+    name = "wxparaver";
+    paths = [ paraver ];
+    buildInputs = [ pkgs.makeWrapper pkgs.installShellFiles ];
+    postBuild = ''
+      wrapProgram "$out/bin/wxparaver" --set GTK_THEME "Adwaita:dark"
+      installManPage $out/share/doc/wxparaver_help_contents/man/*
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
-    wxparaver
+    wxparaver-adawaita
     slack
     openfortivpn
   ];
