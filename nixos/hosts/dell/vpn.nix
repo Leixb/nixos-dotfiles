256a28ed0d101ac5c7ee5c2210996e49603e1f7f
aleixbonerib <aleix.boneribo@bsc.es>
2024-06-15 20:09:02 +0200

    dell: set up vpn

---
+++ nixos/hosts/dell/vpn.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+{
+  sops.secrets.openfortivpn_config = {
+    sopsFile = ./secrets_work/vpn.yaml;
+    owner = "root";
+  };
+
+  systemd.services.openfortivpn = {
+    enable = true;
+    description = "OpenFortiVPN client";
+    documentation = [ "https://github.com/adrienverge/openfortivpn" ];
+
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" "systemd-networkd-wait-online.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "notify";
+      PrivateTmp = true;
+      ExecStart = "${lib.getExe pkgs.openfortivpn} -c ${config.sops.secrets.openfortivpn_config.path}";
+      Restart = "on-failure";
+      OOMScoreAdjust = -100;
+      User = "root";
+      Group = "root";
+    };
+  };
+}
