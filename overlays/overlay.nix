ebdb811f9dd07f5f6d65fde408a01f5bb252ba84
Aleix Boné <aleix.boneribo@bsc.es>
2024-07-12 22:44:52 +0200

    fix: make adwaita wrapper get binPath properly

--- overlays/overlay.nix
+++ overlays/overlay.nix
@@ -1,11 +1,15 @@
 final: prev:
-{
+let lib = prev.lib; in {
   wrapGTKTheme = theme: package: final.symlinkJoin {
     name = "${package.name}-${theme}";
     paths = [ package ];
     nativeBuildInputs = [ final.makeWrapper ];
-    postBuild = ''
-      wrapProgram "${final.lib.getExe package}" --set GTK_THEME "${theme}"
-    '';
+    postBuild =
+      let
+        binPath = lib.removePrefix (builtins.toString (lib.getBin package)) (lib.getExe package);
+      in
+      ''
+        wrapProgram "$out${binPath}" --set GTK_THEME "${theme}"
+      '';
   };
 }
