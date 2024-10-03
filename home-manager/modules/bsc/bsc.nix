b8cecd713948ceb71bf1c9af03352fd1ff12d00e
Aleix Boné <aleix.boneribo@bsc.es>
2024-10-03 12:59:19 +0200

    Add mn5 allocation script

--- home-manager/modules/bsc/bsc.nix
+++ home-manager/modules/bsc/bsc.nix
@@ -1,7 +1,43 @@
 { config, lib, pkgs, ... }:
+let
+  # allocate marenostrum node or reuse previous allocation
+  qalloc = pkgs.writeShellScriptBin "qalloc" ''
+    set -eo pipefail
+
+    HOURS="''${1:-9}"
+    QUEUE=gp_"''${2:-bsccs}"
+
+
+    old="$(tail -n1 ~/.ssh/known_hosts | cut -d' ' -f1)"
+    queue="$(ssh mn5 squeue | tail -n1 | awk '{ print $8 }')"
+
+    if [ "$old" == "$queue" ]; then
+        echo "Reusing allocation $old" >&2
+        node="$old"
+    else
+        echo "Requesting $HOURS hours allocation @ $QUEUE" >&2
+
+        OUT="$(mktemp)"
+        function cleanup {
+            rm -f "$OUT"
+        }
+        trap cleanup EXIT
+
+        ssh mn5 salloc -A bsc15 --qos "$QUEUE" --exclusive --time=$HOURS:00:00  --no-shell |& tee "$OUT"
+
+        node="$(tail -n1 "$OUT" | cut -f 3 -d' ')"
+        mn5_key="$(cat ${config.sops.secrets.ssh_mn5_pubkey.path})"
+
+        echo "$node $mn5_key" >>~/.ssh/known_hosts
+    fi
+
+    exec ssh "$node"
+  '';
+in
 {
   home.packages = with pkgs; [
     msmtp
+    qalloc
     wxparaver-adwaita
     slack
     rocketchat-desktop
@@ -26,6 +62,7 @@
   };
 
   sops.secrets.ssh_config_bsc.path = "${config.home.homeDirectory}/.ssh/config.d/bsc";
+  sops.secrets.ssh_mn5_pubkey.path = "${config.home.homeDirectory}/.ssh/.mn5_pubkey";
 
   programs.git.userName = "Aleix Boné";
   programs.git.includes = [{ path = config.sops.secrets.git_config_bsc.path; }];
