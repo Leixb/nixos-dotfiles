419392826f8d8ad75cd38baab14e8c2d12e7cb02
Aleix Boné <aleix.boneribo@bsc.es>
2025-01-16 16:22:16 +0100

    xmonad: lint

--- home-manager/modules/xmonad.hs
+++ home-manager/modules/xmonad.hs
@@ -47,7 +47,7 @@ import XMonad.Layout.ThreeColumns
 import XMonad.Layout.TwoPanePersistent (TwoPanePersistent (TwoPanePersistent))
 import XMonad.Layout.WorkspaceDir
 import XMonad.Prelude
-import XMonad.Prompt (amberXPConfig, complCaseSensitivity, ComplCaseSensitivity (CaseInSensitive))
+import XMonad.Prompt (ComplCaseSensitivity (CaseInSensitive), amberXPConfig, complCaseSensitivity)
 import XMonad.StackSet (RationalRect (RationalRect))
 import XMonad.StackSet qualified as W
 import XMonad.Util.ClickableWorkspaces (clickablePP)
@@ -295,7 +295,7 @@ myKeys c =
             , ("M-'", addName "Mark Boring" $ markBoringEverywhere)
             , ("M-S-'", addName "Clear Boring" $ clearBoring)
             , ("M-y", addName "Rename workspace" $ renameWorkspace def)
-            , ("M-S-y", addName "Change workspace dir" $ changeDir def {complCaseSensitivity = CaseInSensitive})
+            , ("M-S-y", addName "Change workspace dir" $ changeDir def{complCaseSensitivity = CaseInSensitive})
             ]
             ^++^ subKeys
                 "Volume"
