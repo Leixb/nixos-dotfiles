d918855f1379138b5f3d4fca516daca9069c9e76
Aleix Boné <aleix.boneribo@bsc.es>
2025-04-11 11:15:47 +0200

    Fix hut cache

--- nixos/modules/hut-substituter.nix
+++ nixos/modules/hut-substituter.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   nix.settings = {
-    trusted-substituters = [ "https://jungle.bsc.es/cache" ];
+    substituters = [ "https://jungle.bsc.es/cache" ];
     trusted-public-keys = [ "jungle.bsc.es:pEc7MlAT0HEwLQYPtpkPLwRsGf80ZI26aj29zMw/HH0=" ];
   };
 }
