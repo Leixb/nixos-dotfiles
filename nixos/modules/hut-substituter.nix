233731c46132d5496769ddad7124b1c10dd3983a
Aleix Boné <aleix.boneribo@bsc.es>
2025-06-11 17:46:06 +0200

    Use extra- to avoid overriding substituters

--- nixos/modules/hut-substituter.nix
+++ nixos/modules/hut-substituter.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   nix.settings = {
-    substituters = [ "https://jungle.bsc.es/cache" ];
-    trusted-public-keys = [ "jungle.bsc.es:pEc7MlAT0HEwLQYPtpkPLwRsGf80ZI26aj29zMw/HH0=" ];
+    extra-substituters = [ "https://jungle.bsc.es/cache" ];
+    extra-trusted-public-keys = [ "jungle.bsc.es:pEc7MlAT0HEwLQYPtpkPLwRsGf80ZI26aj29zMw/HH0=" ];
   };
 }
