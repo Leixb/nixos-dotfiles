b100e4cdd9bc11c5d2732060087bda9306a44a03
Aleix Boné <aleix.boneribo@bsc.es>
2025-07-20 21:09:58 +0200

    bsc: relaxed sandbox

--- nixos/hosts/dell/configuration.nix
+++ nixos/hosts/dell/configuration.nix
@@ -113,6 +113,8 @@
     "/sys/devices/system/node"
   ];
 
+  nix.settings.sandbox = "relaxed";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
