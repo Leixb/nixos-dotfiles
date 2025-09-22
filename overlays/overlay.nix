37285839f4d619f0258ce30bf55316e0e74c2c56
Aleix Boné <aleix.boneribo@bsc.es>
2025-09-22 21:39:27 +0200

    style: reformat overlay.nix

--- overlays/overlay.nix
+++ overlays/overlay.nix
@@ -1,16 +1,22 @@
 final: prev:
-let lib = prev.lib; in {
-  wrapGTKTheme = theme: package: final.symlinkJoin {
-    name = "${package.name}-${theme}";
-    paths = [ package ];
-    nativeBuildInputs = [ final.makeWrapper ];
-    postBuild =
-      let
-        binPath = lib.removePrefix (builtins.toString (lib.getBin package)) (lib.getExe package);
-      in
-      ''
-        wrapProgram "$out${binPath}" --set GTK_THEME "${theme}"
-      '';
+let
+  lib = prev.lib;
+in
+{
+  wrapGTKTheme =
+    theme: package:
+    final.symlinkJoin {
+      name = "${package.name}-${theme}";
+      paths = [ package ];
+      nativeBuildInputs = [ final.makeWrapper ];
+      postBuild =
+        let
+          binPath = lib.removePrefix (builtins.toString (lib.getBin package)) (lib.getExe package);
+        in
+        ''
+          wrapProgram "$out${binPath}" --set GTK_THEME "${theme}"
+        '';
+    };
 
   vimPlugins = prev.vimPlugins // {
     fzf-lua = prev.vimPlugins.fzf-lua.overrideAttrs (oldAttrs: {
