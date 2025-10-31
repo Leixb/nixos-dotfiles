48e521322cc74f9fc177f20dc5b983fa7f872517
Aleix Boné <aleix.boneribo@bsc.es>
2025-10-31 20:38:43 +0100

    Add nh (yet another nix helper)

--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -347,6 +347,13 @@ in
       };
     };
 
+  programs.nh = {
+    enable = true;
+    # clean.enable = true;
+    # clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/leix/.dotfiles";
+  };
+
   system.configurationRevision = self.rev or "dirty";
 
   boot.supportedFilesystems = [ "ntfs" ];
