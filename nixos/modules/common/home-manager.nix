39fe54c1a86dbed3a85a6b6c7487e60421fa5d41
Aleix Boné <abone9999@gmail.com>
2026-01-21 15:26:54 +0100

    Revert "feat: add autofirma"
    This reverts commit e8ea7266d63190fd2ad133124346a638ed0c6d2e.

--- nixos/modules/common/home-manager.nix
+++ nixos/modules/common/home-manager.nix
@@ -4,8 +4,6 @@
     inputs.home-manager.nixosModules.home-manager
   ];
 
-  home-manager.extraSpecialArgs = { inherit inputs; };
-
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
 
