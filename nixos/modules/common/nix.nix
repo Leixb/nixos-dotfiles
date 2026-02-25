636167b8a98a7b2dee91bfa6c07f250931f051c5
Aleix Boné <abone9999@gmail.com>
2026-02-25 15:27:50 +0100

    Remove ca-derivations

--- nixos/modules/common/nix.nix
+++ nixos/modules/common/nix.nix
@@ -5,7 +5,7 @@
   nix = {
     package = pkgs.nixVersions.latest;
     extraOptions = ''
-      experimental-features = nix-command flakes ca-derivations
+      experimental-features = nix-command flakes
       keep-outputs = true
       keep-derivations = true
     '';
