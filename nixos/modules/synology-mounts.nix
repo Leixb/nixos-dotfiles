256e51f9fb008a5a0e5f0e37d7483642871cacf4
Aleix Boné <abone9999@gmail.com>
2026-03-10 21:43:41 +0100

    net: update network mount IPs

--- nixos/modules/synology-mounts.nix
+++ nixos/modules/synology-mounts.nix
@@ -8,37 +8,37 @@
   };
 
   fileSystems."/mnt/synology/books" = {
-    device = "192.168.1.3:/volume1/books";
+    device = "192.168.8.3:/volume1/books";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
 
   fileSystems."/mnt/synology/music" = {
-    device = "192.168.1.3:/volume1/music";
+    device = "192.168.8.3:/volume1/music";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
 
   fileSystems."/mnt/synology/photo" = {
-    device = "192.168.1.3:/volume1/photo";
+    device = "192.168.8.3:/volume1/photo";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
 
   fileSystems."/mnt/synology/downloads" = {
-    device = "192.168.1.3:/volume1/Downloads";
+    device = "192.168.8.3:/volume1/Downloads";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
 
   fileSystems."/mnt/synology/shared" = {
-    device = "192.168.1.3:/volume1/Shared folder";
+    device = "192.168.8.3:/volume1/Shared folder";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
 
   fileSystems."/mnt/synology/docker" = {
-    device = "192.168.1.3:/volume1/docker";
+    device = "192.168.8.3:/volume1/docker";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
