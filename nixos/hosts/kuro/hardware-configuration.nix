2c75a300d5e1f8a5ee18acbfe2bdb108352ec912
Aleix Boné <abone9999@gmail.com>
2026-03-10 21:44:07 +0100

    kuro: add nixos.hostPlatform

--- nixos/hosts/kuro/hardware-configuration.nix
+++ nixos/hosts/kuro/hardware-configuration.nix
@@ -68,4 +68,6 @@
   powerManagement.cpuFreqGovernor = "performance";
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
