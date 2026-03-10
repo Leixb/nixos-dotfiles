feeb3dee9e4a998187c0d796675300024761e783
Aleix Boné <abone9999@gmail.com>
2026-03-10 21:52:25 +0100

    fix: rename option programs.git.userName

--- home-manager/modules/personal_git.nix
+++ home-manager/modules/personal_git.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  programs.git.userName = "LeixB";
+  programs.git.settings.user.name = "LeixB";
   programs.git.includes = [{ path = config.sops.secrets.git_config.path; }];
   sops.secrets.git_config.path = "${config.xdg.configHome}/git/config.d/secret.inc";
 }
