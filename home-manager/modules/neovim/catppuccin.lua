e11f5a1fd7a299758aa662e98bcf1d3f06276375
Aleix Boné <abone9999@gmail.com>
2026-05-29 15:54:03 +0200

    nvim: change split line color

--- home-manager/modules/neovim/catppuccin.lua
+++ home-manager/modules/neovim/catppuccin.lua
@@ -1,6 +1,11 @@
 require("catppuccin").setup({
     flavour = "macchiato", -- latte, frappe, macchiato, mocha
     compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+    custom_highlights = function(colors)
+        return {
+            WinSeparator = { fg = colors.blue },
+        }
+    end
 })
 
 vim.cmd.colorscheme("catppuccin")
